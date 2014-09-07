7c484bffb35f067664434954a48475a99bfd2069
Jose Alonso Monge <josmonver@gmail.com>
2014-09-07 20:15:41 +0200

    Added Startup class

---
+++ AngularTokenAuth.Api/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Owin;
+using Microsoft.Owin;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(AngularTokenAuth.Api.Startup))]
+namespace AngularTokenAuth.Api
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            app.UseWebApi(config);
+        }
+    }
+}
