95be5ce4366c52875c66dc2615686b9a3524fffd
Jose Alonso Monge <josmonver@gmail.com>
2014-09-07 20:47:30 +0200

    Create AppDbContext class

---
+++ AngularTokenAuth.Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AngularTokenAuth.Data
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext() :
+            base("AppDbContext")
+        {
+
+        }
+    }
+}
