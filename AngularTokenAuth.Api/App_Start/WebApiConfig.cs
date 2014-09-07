851cd6086047e7f0cbbdfe0d7612cca0615d8bc9
Jose Alonso Monge <josmonver@gmail.com>
2014-09-07 20:19:51 +0200

    JSON format  for returns of api calls

--- AngularTokenAuth.Api/App_Start/WebApiConfig.cs
+++ AngularTokenAuth.Api/App_Start/WebApiConfig.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
 using System.Web.Http;
 
 namespace AngularTokenAuth.Api
@@ -19,6 +21,9 @@ namespace AngularTokenAuth.Api
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
         }
     }
 }
